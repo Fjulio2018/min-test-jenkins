18bebc4c309141916806b10e077fc434a98fd5d6
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-17 10:44:40 -0300

    Podemos rodar no jankins II

--- src/test/java/ui_Testing/TestNG.java
+++ src/test/java/ui_Testing/TestNG.java
@@ -1,12 +1,15 @@
 package ui_Testing;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class TestNG {
     WebDriver driver;
@@ -14,18 +17,21 @@ public class TestNG {
     @BeforeSuite
     public void setUp() {
 
-        String caminhoDoChromeDriver = "src/test/resources/drivers/chromedriver.exe";  // Assuming this is the correct path
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-
-        System.setProperty("webdriver.chrome.driver", caminhoDoChromeDriver);  // Fix property name
-
-        driver = new ChromeDriver(chromeOptions);
     }
-
     @Test
-    public void openWindow() {
+    public void subirPagona(){
         driver.navigate().to("https://blog.testproject.io/");
-        System.out.println("To Title:  " + driver.getTitle());
+        System.out.println("To Title: " + driver.getTitle());
+
     }
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+
+
 }
